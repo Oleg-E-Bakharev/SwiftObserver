a747a8b3c2f3ac4093ba17b768d9a72f81c791be
Олег Бахарев <Oleg.Bakharev@mvideo.ru>
2026-01-08 01:16:45 +0300

    Добавлено указание минимальных версий OS

--- Package.swift
+++ Package.swift
@@ -1,13 +1,13 @@
-// swift-tools-version:6.0
+// swift-tools-version:6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftObserver",
-//    platforms: [
-//        .iOS(.v15),
-//    ],
+    platforms: [
+        .iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .macCatalyst(.v13), .visionOS(.v1), .watchOS(.v6)
+    ],
     products: [
         .library(
             name: "SwiftObserver",
